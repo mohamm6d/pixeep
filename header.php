01974cc9e061f6c0c49f399f5dcbc2d075bda771
mohadiz <mohammad@lync.me>
2020-08-24 19:34:41 +0300

    rdm

--- header.php
+++ header.php
@@ -25,6 +25,10 @@
 </head>
 
 <body <?php body_class(); ?>>
+<?php do_action('after_body_open_tag'); ?>
+
+
+
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'acoustics' ); ?></a>
     <?php do_action( 'acoustics_before_header' ); ?>
